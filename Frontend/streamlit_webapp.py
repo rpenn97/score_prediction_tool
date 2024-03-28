a0ac0d0c8e02d027e5395e87c2ca56d2116da4a4
Reece Penn <reecepenn97@gmail.com>
2024-03-28 17:09:32 +0000

    Streamlit EOD 28 Mar

--- Frontend/streamlit_webapp.py
+++ Frontend/streamlit_webapp.py
@@ -14,17 +14,20 @@ st.title('Football Predictions Across Top 5 Leagues')
 
 st.markdown("""
 * **Leagues:** Premier League, Ligue 1, Bundesliga, Serie A, La Liga
-* **Data Source:**understat.com            """)
+* **Data Source:** www.understat.com            """)
 
 st.sidebar.header("User Input")
 
 sorted_unique_match = sorted(scoresheet_df["Team"].unique())
 selected_match = st.sidebar.selectbox('Match',sorted_unique_match)
 
-nation = sorted(scoresheet_df['Nation'].unique())
-selected_nation = st.sidebar.multiselect('Nation',nation,nation)
+# nation = sorted(scoresheet_df['Nation'].unique())
+# selected_nation = st.sidebar.multiselect('Nation',nation,nation)
+# print(selected_match)
+# print(selected_nation)
 
-scoresheet_df_filt = scoresheet_df[(scoresheet_df.Tm.isin(selected_nation)) & (scoresheet_df.Pos.isin(selected_match))]
+scoresheet_df_filt = scoresheet_df.loc[scoresheet_df['Team'] == selected_match]
+# scoresheet_df_filt = scoresheet_df[(scoresheet_df.Nation.isin(selected_nation)) & (scoresheet_df.Team.isin(sorted_unique_match))]
 
 st.header('Predictions this week:')
 st.dataframe(scoresheet_df_filt)
